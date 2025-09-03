5203638c7dd7ddb8671a90e1a56455bcb4672abc
Christiana Cho <christianacho@Christianas-MacBook-Air-2.local>
2025-09-03 16:10:38 -0700

    Home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to My App 🚀</h1>
+      <p>Hi</p>
+    </div>
+  );
+}
