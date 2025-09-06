e0f49a8940114eddbe3e8d7bd10547e439e31e53
Christiana Cho <christianacho@mac.local.ftr>
2025-09-06 15:49:39 -0700

    Navbar edits, FAQ page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Faq from "./pages/Faq";
 import "./App.css";
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/faq" element={<Faq />} />
     </Routes>
+    </>
   );
 }
 
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,31 +1,35 @@
 import React from 'react';
+import Navbar from "./Navbar";
 import '../style/Login.css';
 
 const LoginForm = () => {
     return (
-        <div className='wrapper'>
-            <form action="">
-                <h1>Login</h1>
-                <div className="input-box">
-                    <input type="text" placeholder='Username' required />
-                </div>
-                <div className="input-box">
-                    <input type="passsword" placeholder='Password' required />
-                </div>
+        <div className="container">
+            <Navbar />
+            <div className='wrapper'>
+                <form action="">
+                    <h1>Login</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder='Username' required />
+                    </div>
+                    <div className="input-box">
+                        <input type="passsword" placeholder='Password' required />
+                    </div>
 
-                <div className="remember-forgot">
-                    <label><input type="checkbox" /> Remember me</label>
-                    <a href="#">Forgot password?</a>
-                </div>
+                    <div className="remember-forgot">
+                        <label><input type="checkbox" /> Remember me</label>
+                        <a href="#">Forgot password?</a>
+                    </div>
 
-                <button type="submit">Login</button>
+                    <button type="submit">Login</button>
 
-                <div className="register-link">
-                    <p>Don't have an account? <a href="#">Register</a></p>
-                </div>
-            </form>
-        
-        
+                    <div className="register-link">
+                        <p>Don't have an account? <a href="#">Register</a></p>
+                    </div>
+                </form>
+            
+            
+            </div>
         </div>
     );
 };
